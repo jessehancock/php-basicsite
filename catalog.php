48cfd98f37df5de8d20d7345430c7bc61aba3e23
jessehancock <jessehancock1@gmail.com>
2016-12-07 11:20:15 -0700

    added data

--- catalog.php
+++ catalog.php
@@ -1,4 +1,8 @@
 <?php
+include("inc/data.php");
+  // adds new element to an array;
+
+
 $pageTitle = "Full Catalog";
 $section = null;
 
@@ -19,8 +23,20 @@ if (isset($_GET["cat"])){
 
 include 'inc/header.php'; ?>
 
-<div class="section page">
+<div class="section catalog page">
   <h1><?php echo $pageTitle; ?></h1>
+  <ul class="items">
+    <?php
+    foreach($catalog as $item) {
+      echo "<li><a href='#'> <img src=' "
+      . $item["img"] . "' alt='"
+      . $item["title"] . "' />"
+      . "<p>View Details</p>"
+      ."</a></li>";
+    }
+    ?>
+  </ul>
+
 
 </div>
 
